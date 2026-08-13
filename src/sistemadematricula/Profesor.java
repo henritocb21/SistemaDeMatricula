d8c3590f6326c0a7d17c79db6065778df334ee27
Student <Student@LITISGPCJDG5DB4>
2026-08-13 14:25:59 -0600

    Se sube la rama profesor

---
+++ src/sistemadematricula/Profesor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemadematricula;
+
+/**
+ *
+ * @author Student
+ */
+public class Profesor {
+   private String nombre;
+   private int cedula;
+   private int edad;
+   private int telefono;
+}
