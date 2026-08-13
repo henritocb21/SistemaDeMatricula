92c08a0167805d335a9f423a1b47e8a35d88c519
Student <Student@LITISGPCJDG5DB4>
2026-08-13 14:32:40 -0600

    Java Class actualizado

--- src/sistemadematricula/Profesor.java
+++ src/sistemadematricula/Profesor.java
@@ -9,8 +9,56 @@ package sistemadematricula;
  * @author Student
  */
 public class Profesor {
-   private String nombre;
-   private int cedula;
-   private int edad;
-   private int telefono;
+
+    private String nombre;
+    private int cedula;
+    private int edad;
+    private int telefono;
+
+    public Profesor(String nombre, int cedula, int edad, int telefono) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.edad = edad;
+        this.telefono = telefono;
+    }
+
+    public Profesor() {
+        this.nombre = "";
+        this.cedula = 0;
+        this.edad = 0;
+        this.telefono = 0;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
 }
